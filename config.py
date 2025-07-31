d5871735a419e641d342f4f9d4c8bf91e99579b2
minhhiennvu <minhhiennvu@gmail.com>
2025-07-31 17:54:26 +0700

    config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/1400140735805653113/jWXy-dUWiT-wrgNw6SrWEtvHVP7fbaqRZoz_6j9-KovsvNrtbFDRgjz4I6f1aDaOoU8B"
+
+ACCOUNTS = [
+    {"name": "T1 Gumayusi", "tag": "KR1", "encoded": "T1%20Gumayusi", "server": "kr"},
+    {"name": "구르시", "tag": "녹서스", "encoded": "%EA%B5%AC%EB%A5%B4%EC%8B%9C", "server": "kr"},
+]
